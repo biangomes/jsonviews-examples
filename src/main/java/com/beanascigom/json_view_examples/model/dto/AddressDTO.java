13be5de64d302b742a8d03830d3a9f78fec895e7
Beatriz Gomes <beanascigom@gmail.com>
2024-10-24 00:55:39 -0300

    Refatoração do AddressDTO utilizando agora Getter e Setter do Lombok

--- src/main/java/com/beanascigom/json_view_examples/model/dto/AddressDTO.java
+++ src/main/java/com/beanascigom/json_view_examples/model/dto/AddressDTO.java
@@ -1,12 +1,14 @@
 package com.beanascigom.json_view_examples.model.dto;
 
 import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class AddressDTO {
   private Long id;
-
   private String zipCode;
-
   private String street;
   private Integer number;
   private String neighborhood;
@@ -14,57 +16,7 @@ public class AddressDTO {
   @Column(length = 2)
   private String state;
   private String country;
-  public String getZipCode() {
-    return zipCode;
-  }
 
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public void setZipCode(String zipCode) {
-    this.zipCode = zipCode;
-  }
-
-  public String getStreet() {
-    return street;
-  }
-
-  public void setStreet(String street) {
-    this.street = street;
-  }
-
-  public Integer getNumber() {
-    return number;
-  }
-
-  public void setNumber(Integer number) {
-    this.number = number;
-  }
-
-  public String getNeighborhood() {
-    return neighborhood;
-  }
-
-  public void setNeighborhood(String neighborhood) {
-    this.neighborhood = neighborhood;
-  }
-
-  public String getCity() {
-    return city;
-  }
-
-  public void setCity(String city) {
-    this.city = city;
-  }
-
-  public String getState() {
-    return state;
-  }
 
   public void setState(String state) {
     if (!state.matches("[A-Za-z ]*")) {
@@ -73,11 +25,4 @@ public class AddressDTO {
     this.state = state;
   }
 
-  public String getCountry() {
-    return country;
-  }
-
-  public void setCountry(String country) {
-    this.country = country;
-  }
 }
